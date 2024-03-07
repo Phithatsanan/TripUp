05919f591658915e920552e62870f69615035549
kittitater <kittitater@gmail.com>
2024-03-08 02:54:58 +0700

    update home

--- tripup-frontend/src/pages/Home.tsx
+++ tripup-frontend/src/pages/Home.tsx
@@ -14,8 +14,10 @@ export default function Home() {
     <>
       <Layout>
 
-        <div className="py-60 px-10 md:px-20 sm:py-60 bg-center  bg-cover bg-[url('/src/assets/home-bg-phuket.jpeg')]" >
-          <h1 className="mb-6 text-4xl  font-medium leading-none tracking-tight text-white md:text-5xl lg:text-6xl dark:text-white">Best Destinations to Experience</h1>
+        <div className="py-52 px-10 md:px-20 sm:py-50 bg-center  bg-cover bg-[url('/src/assets/home-bg-phuket.jpeg')]" >
+          <h1 className=" text-4xl  font-medium leading-none tracking-tight text-white md:text-5xl lg:text-6xl dark:text-white">Best Destinations to Experience</h1>
+          <h1 className="mb-6 text-4xl  font-medium leading-none tracking-tight text-white md:text-5xl lg:text-6xl dark:text-white">Less Experience to Discover</h1>
+
           {/* <p className="mb-6 text-lg font-normal text-white lg:text-xl text-center dark:text-gray-400">Here at TripUp we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p> */}
           <Link to="/discover" className="inline-flex items-center px-5 py-3 text-base font-medium text-center text-gray-800 bg-[#98DB2E] rounded-lg hover:bg-[#99db2eca] " >
             Discover
@@ -28,7 +30,7 @@ export default function Home() {
         {/* <img className="h-auto max-w-full" src="https://www.letsphuket.com/wp-content/uploads/phuket1.jpg" alt="image description" /> */}
 
         <section className="bg-white dark:bg-gray-900">
-          <div className="max-w-screen-xl px-8 py-8 mx-auto text-center lg:py-16 lg:px-6">
+          <div className="max-w-screen-xl px-8 py-24 mx-auto text-center lg:py-16 lg:px-6 sm:py-8">
             <figure className="max-w-screen-md mx-auto">
               <svg className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" />
