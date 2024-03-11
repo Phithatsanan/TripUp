e3f692d7561368ec85c1ee702cec6cde7f2cc5ec
kittitater <kittitater@gmail.com>
2024-03-12 01:45:35 +0700

    continue edit

--- tripup-frontend-next/src/app/page.tsx
+++ tripup-frontend-next/src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
                 <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" />
               </svg>
               <blockquote>
-                <p className="text-sm sm:text-xl  font-medium text-gray-900 dark:text-white">" TripUp is set to revolutionize the travel planning process by introducing a streamlined, efficient, and integrated platform. Our goal is to create a game-changing web app that transforms the way people plan their trips."</p>
+                <p className="text-sm sm:text-xl font-medium text-gray-900 dark:text-white">&quot;TripUp is set to revolutionize the travel planning process by introducing a streamlined, efficient, and integrated platform. Our goal is to create a game-changing web app that transforms the way people plan their trips.&quot;</p>
               </blockquote>
               <figcaption className="flex items-center justify-center mt-6 space-x-3">
                 {/* <img className="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture"> */}
