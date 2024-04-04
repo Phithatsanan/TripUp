f4c2450ac09c0be6a3c2d04bb0cbd088e7c0ae2a
kittitater <kittitater@gmail.com>
2024-04-04 10:42:03 +0700

    remove next code

--- tripup-frontend/src/pages/Layout.tsx
+++ tripup-frontend/src/pages/Layout.tsx
@@ -5,12 +5,12 @@ import NavBar from "../components/Navbar";
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div>
-            <NavBar/>
+            <NavBar />
             <main>
                 {/* Render the children components */}
                 {children}
             </main>
-            <Footer/>
+            <Footer />
         </div>
     );
 };
